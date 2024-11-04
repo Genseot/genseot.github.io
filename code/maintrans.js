97549bac742795ebd116fe5f0565cb3da2b4bc2b
Genseot <genseot2@gmail.com>
2024-11-04 17:41:35 -0500

    Update maintrans.js

--- code/maintrans.js
+++ code/maintrans.js
@@ -2,24 +2,16 @@
 window.transitionTo = function(href) {document.querySelector('body');
 setTimeout(function() {window.location.href = href})};
 
-function loadIndex() {const xhttp = new XMLHttpRequest();
+const xhttp = new XMLHttpRequest();
+function loadIndex() {
 xhttp.onload = function() {document.getElementById("storylet").innerHTML = this.responseText;};
 xhttp.open("GET", "index.txt");
 xhttp.send();}
-function loadAbout() {const xhttp = new XMLHttpRequest();
+function loadAbout() {
 xhttp.onload = function() {document.getElementById("storylet").innerHTML = this.responseText;};
 xhttp.open("GET", "about.txt");
 xhttp.send();}
-function loadStories() {const xhttp = new XMLHttpRequest();
+function loadStories() {
 xhttp.onload = function() {document.getElementById("storylet").innerHTML = this.responseText;};
 xhttp.open("GET", "stories.txt");
 xhttp.send();}
-
-/*function loadBeyondGatesDream() {const xhttp = new XMLHttpRequest();
-xhttp.onload = function() {document.getElementById("storylet").innerHTML = this.responseText;};
-xhttp.open("GET", "constellarium.txt");
-xhttp.send();}
-function loadCrystallineHeart() {const xhttp = new XMLHttpRequest();
-xhttp.onload = function() {document.getElementById("storylet").innerHTML = this.responseText;};
-xhttp.open("GET", "about.txt");
-xhttp.send();}*/
