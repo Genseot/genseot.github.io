11cab3eb0a658707dfc1bbe0ce801cbb02854dc1
Genseot <genseot2@gmail.com>
2025-03-13 23:10:06 +0000

    Update maintrans.js

--- code/maintrans.js
+++ code/maintrans.js
@@ -2,16 +2,23 @@
 window.transitionTo = function(href) {document.querySelector('body');
 setTimeout(function() {window.location.href = href})};
 
-const xhttp = new XMLHttpRequest();
-function loadIndex() {
-xhttp.onload = function() {document.getElementById("storylet").innerHTML = this.responseText;};
-xhttp.open("GET", "index.txt");
-xhttp.send();}
-function loadAbout() {
-xhttp.onload = function() {document.getElementById("storylet").innerHTML = this.responseText;};
-xhttp.open("GET", "about.txt");
-xhttp.send();}
-function loadStories() {
-xhttp.onload = function() {document.getElementById("storylet").innerHTML = this.responseText;};
-xhttp.open("GET", "stories.txt");
-xhttp.send();}
+async function loadIndex() {
+  await fetch("index.txt")
+  .then(response => response.text())
+  .then((data) => { document.getElementById("storylet").innerHTML = data; })
+}
+async function loadAbout() {
+  await fetch("about.txt")
+  .then(response => response.text())
+	.then((data) => { document.getElementById("storylet").innerHTML = data; })
+}
+async function loadStories() {
+  await fetch("stories.txt")
+	.then(response => response.text())
+	.then((data) => { document.getElementById("storylet").innerHTML = data; })
+}
+async function loadMods() {
+  await fetch("mods.txt")
+	.then(response => response.text())
+	.then((data) => { document.getElementById("storylet").innerHTML = data; })
+}
