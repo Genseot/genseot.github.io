b3262544bf8660469291e0f0632cb9c24660527b
Genseot <genseot2@gmail.com>
2025-07-10 00:33:28 -0400

    Update theirstreets.js

--- mods/theirstreets/theirstreets.js
+++ mods/theirstreets/theirstreets.js
@@ -25,8 +25,6 @@ DIALOGUE
 content.insertAdjacentHTML('beforeend', `<link type="text/css" rel="stylesheet" href="https://genseot.github.io/mods/theirstreets/theirstreets.css">`) 
 addResources(["/js/lib/pixi.js","/js/lib/pixi-gif.js",])
 var stageWater
-var getFountainPillar = document.getElementById("fountainpillar26");
-var spawnFountainPillar = `<div id="fountainpillar26" base="fountainpillar" class=" dypcontent " type="" origin-spot="94" style="--piece-delay: -18.626161118958752s; animation-delay: var(--piece-delay);--dyp-image: url(https://genseot.github.io/mods/theirstreets/img/fountainpillar.gif);--dyp-width: 2;--dyp-height: 4; --dyp-transform:rotateY(90deg);"></div>`;
 
 function EpisodeCheck() {
 	if(!check('ep0_epilogue')) {
@@ -441,9 +439,7 @@ env.stages['city_street_fountain'] = {
     exec: ()=>{ 
         page.bgm.rate(1)
         EpisodeCheck()
-	Fountain()
     },
-
     entities: {
         Fb: {
     	    class:"prop",
@@ -524,7 +520,12 @@ env.stages['city_street_fountain'] = {
         '.','Br','░','░','░','░','░','░','░','Br','.',
         '.','tcr','B','░','░','p','░','░','B','tc','.',
         '.','.','.','.','.','m','.','.','.','.','.',
-    ]
+    ],
+    exec: ()=>{
+	var getFountainPillar = document.getElementById("fountainpillar26");
+        var spawnFountainPillar = `<div id="fountainpillar26" base="fountainpillar" class=" dypcontent " type="" origin-spot="94" style="--piece-delay: -18.626161118958752s; animation-delay: var(--piece-delay);--dyp-image: url(https://genseot.github.io/mods/theirstreets/img/fountainpillar.gif);--dyp-width: 2;--dyp-height: 4; --dyp-transform:rotateY(90deg);"></div>`;
+	Fountain()
+    }
 }
 
 
